b3c547af4eba293fca0e5652c2fc2bf979a0be56
Isabella <isabella.y.hu@gmail.com>
2022-09-05 21:35:54 +1000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+class School
+    attr_accessor :roster
+    attr_reader :school
+    
+
+    def initialize(school)
+        @school = school
+        @roster = {}
+    end
+
+    def add_student(student,grade)
+        @roster[grade] || @roster[grade] = []
+        @roster[grade] << student
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        @roster.merge(@roster) {|grade, student| student.sort}
+    end
+
+end
